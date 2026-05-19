20c3dbfceeda45d290e64fe1134942551d2836c7
Jefferson-Palma <jefferson.palma@sptech.school>
2026-05-19 19:54:18 -0300

    validar sessão funcionando

--- public/public/js/sessao.js
+++ public/public/js/sessao.js
@@ -2,37 +2,33 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
-    //var cpf=sessionStorage.CPF_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
     //var b_cpf=document.getElementById('id_cpf');
-    if (email != null && nome != null) {
-        b_usuario.innerHTML = nome;
-      //  b_cpf.innerHTML=cpf; 
-    } else {
-        window.location = "../loginn.html";
+    if (email == null && nome == null) {
+      window.location = "../loginn.html";
     }
 }
 
 function limparSessao() {
     sessionStorage.clear();
-    window.location = "../login.html";
+    window.location = "../loginn.html";
 }
 
-// carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
+// // carregamento (loading)
+// function aguardar() {
+//     var divAguardar = document.getElementById("div_aguardar");
+//     divAguardar.style.display = "flex";
+// }
 
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
+// function finalizarAguardar(texto) {
+//     var divAguardar = document.getElementById("div_aguardar");
+//     divAguardar.style.display = "none";
 
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
-}
+//     var divErrosLogin = document.getElementById("div_erros_login");
+//     if (texto) {
+//         divErrosLogin.style.display = "flex";
+//         divErrosLogin.innerHTML = texto;
+//     }
+// }
 
